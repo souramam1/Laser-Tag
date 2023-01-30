5455eda2366ea98fd3400739e777abc12ba1a013
souramam1 <maia.ramambason20@imperial.ac.uk>
2023-01-30 23:53:36 +0000

    connecting home page to base . html

--- DjangoApp/urls.py
+++ DjangoApp/urls.py
@@ -14,12 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.http.response import HttpResponse
-from django.urls import path,include
+from django.http.response import HttpResponse, Http404, HttpResponseNotFound, Http404
+from django.urls import path,include,reverse
+from django.shortcuts import render
 
 
 def home_view(request):
-    return HttpResponse("HOME PAGE")
+    #return HttpResponse("HOME PAGE")
+    return render(request,'base.html')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
