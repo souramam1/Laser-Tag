91be53a2abf8f2b9fba95213b52f5a519192fe70
souramam1 <maia.ramambason20@imperial.ac.uk>
2023-01-30 20:42:45 +0000

    trying to add a templates folder first commit

--- welcome_page/views.py
+++ welcome_page/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, HttpResponseNotFound, Http404
+from django.urls import reverse
 
 # Create your views here.
 def simple_view(request):
-    return HttpResponse('HELLO')
+    #return HttpResponse('HELLO') 
+    return render(request, 'welcome_page/example.html')  # .html
